f3c1cd1c6871d3d97a5e186a20499a6ba0ed72fc
Wilghaner <wilghener@gmail.com>
2022-11-16 15:42:13 -0300

    make sure call validator with correct email

--- src/utils/helpers/email-validator.spec.ts
+++ src/utils/helpers/email-validator.spec.ts
@@ -10,23 +10,43 @@ class EmailValidator implements iEmailValidator{
   }
 }
 
+const makeSut = () => {
+  const sut = new EmailValidator();
+  return {sut};
+}
+
 describe('Email Validator', () => {
   const mockedValidator = validator as jest.Mocked<typeof validator>;
   
   
   test('should return true if validator return true', async () => {
-    const sut = new EmailValidator();
-    mockedValidator.isEmail.mockImplementation(()=>{return true});
+    const {sut} = makeSut()
+    mockedValidator.isEmail.mockReturnValue(true);
     const isValid = sut.validateEmail('valid_email@mail.com');
 
     expect(isValid).toEqual(true);
   })
 
   test('should return false if validator return false', async () => {
-    const sut = new EmailValidator();
-    mockedValidator.isEmail.mockImplementation(()=>{return false});
+    const {sut} = makeSut()
+    mockedValidator.isEmail.mockReturnValue(false);
     const isValid = sut.validateEmail('invalid_email@mail.com');
     
     expect(isValid).toBe(false);
   })
+
+  test('should call validator with correct email', async () => {
+    const {sut} = makeSut()
+    let calledEmail
+    
+    mockedValidator.isEmail.mockImplementation((email: string)=>{
+      calledEmail = email;
+      return true;
+    })
+    const email = 'any_email@mail.com'
+    sut.validateEmail(email);
+
+    expect(calledEmail).toBe(email);
+  })
+
 });
