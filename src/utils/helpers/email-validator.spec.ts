cd219dddf8ec0256ea4b9798bb9f494f863e0275
Wilghaner <wilghener@gmail.com>
2022-11-17 15:19:02 -0300

    Moving the EmailValidator to its own file

--- src/utils/helpers/email-validator.spec.ts
+++ src/utils/helpers/email-validator.spec.ts
@@ -1,17 +1,8 @@
-import iEmailValidator from '../../interfaces/email-validator';
+import EmailValidator from './email-validator';
 import validator from 'validator';
 import MissingParamError from '../errors/MissingParam';
 
 jest.mock('validator');
-
-class EmailValidator implements iEmailValidator{
-  
-  validateEmail(email: string): boolean {
-    if(!email){throw new MissingParamError('email')}
-  
-    return validator.isEmail("any_email@mail.com");
-  }
-}
 
 const makeSut = () => {
   const sut = new EmailValidator();
