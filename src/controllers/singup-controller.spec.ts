907e81f6394b450320edc945dd900ed0551ee0b1
Wilghaner <wilghener@gmail.com>
2022-11-22 14:44:25 -0300

    Ensuring correct use of singupService

--- src/controllers/singup-controller.spec.ts
+++ src/controllers/singup-controller.spec.ts
@@ -3,20 +3,23 @@ import HttpResponse from '../utils/HttpResponse';
 import Controller from '../utils/interfaces/controller';
 import { HttpReq, HttpRes } from '../utils/types/Http-types';
 import iEmailValidator from '../utils/interfaces/email-validator';
+import iSingupService from '../utils/interfaces/singup-service';
 
 
 class SingupController implements Controller{
-  constructor(private EmailValidator: iEmailValidator){}
+  constructor(private emailValidator: iEmailValidator, private singupService: iSingupService){}
 
   async route(httpRequest: HttpReq): Promise<HttpRes> {
     const {name, email, password} = httpRequest.body;
 
-    if(!email){return HttpResponse.badRequest('email');}
-    if(!name){return HttpResponse.badRequest('name');}
-    if(!password){return HttpResponse.badRequest('password');}
-    if(!this.EmailValidator || !this.EmailValidator.validateEmail){return HttpResponse.serverError();}
-    if(!this.EmailValidator.validateEmail(email)){return HttpResponse.unauthorized('Invalid email');}
-    
+    if(!email){return HttpResponse.badRequest('email')};
+    if(!name){return HttpResponse.badRequest('name')};
+    if(!password){return HttpResponse.badRequest('password')};
+    if(!this.emailValidator || !this.emailValidator.validateEmail){return HttpResponse.serverError()};
+    if(!this.singupService || !this.singupService.sing){return HttpResponse.serverError()};
+
+    if(!this.emailValidator.validateEmail(email)){return HttpResponse.unauthorized('Invalid email')};
+    if(!await this.singupService.sing(name, email, password)){return HttpResponse.unauthorized('Email in use')};    
     return HttpResponse.badRequest('email');
   }
 }
@@ -31,11 +34,27 @@ class EmailValidatorSpy implements iEmailValidator{
   }
 }
 
+class SingupServiceSpy implements iSingupService{
+  public name = '';
+  public email = '';
+  public password = '';
+  public isCreated = true;
+  
+  async sing(name: string, email: string, password: string): Promise<boolean> {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+
+    return this.isCreated;
+  }
+}
+
 const makeSut =  () => {
   const emailValidatorSpy = new EmailValidatorSpy();
-  const sut = new SingupController(emailValidatorSpy);
+  const singupServiceSpy = new SingupServiceSpy();
+  const sut = new SingupController(emailValidatorSpy, singupServiceSpy);
 
-  return {emailValidatorSpy, sut};
+  return {singupServiceSpy, emailValidatorSpy, sut};
 }
 
 describe('Singup Controller', () => {
@@ -82,7 +101,8 @@ describe('Singup Controller', () => {
   })
 
   test('should return 500 if an invalid EmailValidator is provided', async () => {
-    const sut = new SingupController({} as iEmailValidator);
+    const singupServiceSpy = new SingupServiceSpy();
+    const sut = new SingupController({} as iEmailValidator, singupServiceSpy);
     const httpRequest = {
       body:{
         name: 'any_name',
@@ -95,7 +115,7 @@ describe('Singup Controller', () => {
     expect(httpResponse.statusCode).toEqual(500);
   })
 
-  test('should return 401 if EmailValidator retrun false', async () => {
+  test('should return 401 if EmailValidator return false', async () => {
     const {sut, emailValidatorSpy} = makeSut();
     const httpRequest = {
       body: {
@@ -111,4 +131,55 @@ describe('Singup Controller', () => {
     expect(httpResponse.statusCode).toEqual(401);
     expect(httpResponse.body).toEqual('Invalid email');
   })
+
+  test('should return 500 if an invalid singupService is provided', async () => {
+    const emailValidatorSpy = new EmailValidatorSpy();
+    const sut = new SingupController(emailValidatorSpy, {} as iSingupService);
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      }
+    }
+
+    const httpResponse = await sut.route(httpRequest);
+    expect(httpResponse.statusCode).toEqual(500);
+  })
+  
+  test('should return 401 if singupServie return false', async () => {
+    const {sut, singupServiceSpy} = makeSut();
+    
+    const httpResquest = {
+      body: {
+        name: 'any_name',
+        email: 'used_email@mail.com',
+        password: 'any_password',
+      }
+    }
+
+    singupServiceSpy.isCreated = false;
+
+    const httpResponse = await sut.route(httpResquest);
+    expect(httpResponse.statusCode).toEqual(401);
+  })
+
+  test('should call singupService with correct params', async () => {
+    const {sut, singupServiceSpy} = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+
+    await sut.route(httpRequest);
+
+    expect(singupServiceSpy.email).toEqual(httpRequest.body.email);
+    expect(singupServiceSpy.name).toEqual(httpRequest.body.name);
+    expect(singupServiceSpy.password).toEqual(httpRequest.body.password);
+  })
+
 })
---
+++ src/utils/interfaces/singup-service.ts
@@ -0,0 +1,7 @@
+import User from '../types/user-type';
+
+interface iSingupService {
+  sing(name: string, email: string, password: string): Promise<boolean>
+}
+
+export default iSingupService;
