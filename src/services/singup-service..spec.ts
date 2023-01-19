d514eeb34f2f6cf91b90c045f7fe3ccbb533d53e
WilghanerSilva <wilghener@gmail.com>
2023-01-19 13:06:35 -0300

    moving the code to their respective files

--- src/services/singup-service..spec.ts
+++ src/services/singup-service..spec.ts
@@ -1,32 +1,7 @@
 import MissingParamError from '../utils/errors/MissingParam';
-import { iLoadUserByEmailRepository, iSingupService, iCreateUserRepository } from "../utils/interfaces";
+import SingupService from "./singup-service";
+import { iLoadUserByEmailRepository, iCreateUserRepository } from "../utils/interfaces";
 import User from '../utils/types/user-type';
-
-class SingupService implements iSingupService {
-  constructor(
-    private readonly loadUserByEmailRepository: iLoadUserByEmailRepository,
-    private readonly createUserRepository: iCreateUserRepository,
-    ){}
-
-  async sing(name: string, email: string, password: string): Promise<boolean> {
-    if(!name){throw new MissingParamError('name')};
-    if(!email){throw new MissingParamError('email')};
-    if(!password){throw new MissingParamError('password')};
-    
-    if(!this.loadUserByEmailRepository || !this.loadUserByEmailRepository.load)
-      throw new Error("Invalid LoadUserByEmailRepository");
-    
-    if(!this.createUserRepository || !this.createUserRepository.create)
-      throw new Error("Invalid CreateUserRepository");
-
-    if(!!this.loadUserByEmailRepository.load(email))
-      return false;
-    
-    
-    
-    return true;
-  }
-}
 
 class LoadUserByEmailRepositorySpy implements iLoadUserByEmailRepository{
   public user: User | null | undefined = null;
---
+++ src/services/singup-service.ts
@@ -0,0 +1,30 @@
+import MissingParamError from '../utils/errors/MissingParam';
+import { iLoadUserByEmailRepository, iSingupService, iCreateUserRepository } from "../utils/interfaces";
+
+class SingupService implements iSingupService {
+  constructor(
+    private readonly loadUserByEmailRepository: iLoadUserByEmailRepository,
+    private readonly createUserRepository: iCreateUserRepository,
+    ){}
+
+  async sing(name: string, email: string, password: string): Promise<boolean> {
+    if(!name){throw new MissingParamError('name')};
+    if(!email){throw new MissingParamError('email')};
+    if(!password){throw new MissingParamError('password')};
+    
+    if(!this.loadUserByEmailRepository || !this.loadUserByEmailRepository.load)
+      throw new Error("Invalid LoadUserByEmailRepository");
+    
+    if(!this.createUserRepository || !this.createUserRepository.create)
+      throw new Error("Invalid CreateUserRepository");
+
+    if(!!this.loadUserByEmailRepository.load(email))
+      return false;
+    
+    
+    
+    return true;
+  }
+}
+
+export default SingupService;
