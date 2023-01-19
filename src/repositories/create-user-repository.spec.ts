29d6cfc4ded96920e765d0c016b750e1f9b3eddf
WilghanerSilva <wilghener@gmail.com>
2023-01-19 13:23:51 -0300

    moving the code to its respective file

--- src/repositories/create-user-repository.spec.ts
+++ src/repositories/create-user-repository.spec.ts
@@ -1,22 +1,8 @@
-import { iCreateUserRepository } from "../utils/interfaces";
 import User from '../utils/types/user-type';
 import { prismaMock } from '../../singleton';
-import prisma from "../../client";
 import MissingParamError from '../utils/errors/MissingParam';
+import CreateUserRepository from "./create-user-repository";
 
-class CreateUserRepository implements iCreateUserRepository {
-  async create(user: Omit<User, "id">): Promise<void> {
-    
-    if(!user.email || !user.name || !user.password)
-      throw new MissingParamError("Invalid user has sent");
-    
-    await prisma.user.create({
-            data: {
-        ...user
-      }
-    })
-  }
-}
 
 
 describe('CreateUserRepository', () => {
---
+++ src/repositories/create-user-repository.ts
@@ -0,0 +1,20 @@
+import { iCreateUserRepository } from "../utils/interfaces";
+import prisma from "../../client";
+import User from '../utils/types/user-type';
+import MissingParamError from '../utils/errors/MissingParam';
+
+class CreateUserRepository implements iCreateUserRepository {
+  async create(user: Omit<User, "id">): Promise<void> {
+    
+    if(!user.email || !user.name || !user.password)
+      throw new MissingParamError("Invalid user has sent");
+    
+    await prisma.user.create({
+            data: {
+        ...user
+      }
+    })
+  }
+}
+
+export default CreateUserRepository;
