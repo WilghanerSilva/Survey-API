21807051c3109b8e2a620695bff86f0926198458
WilghanerSilva <wilghener@gmail.com>
2023-01-20 20:49:28 -0300

    ensuring that it throws an error if the token manager is wrong  and that it returns a 401 code if the token is wrong

---
+++ src/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,56 @@
+import {iTokenManager} from '../utils/interfaces';
+import { HttpRes } from '../utils/types/Http-types';
+
+class AuthMiddleware {
+  constructor (private readonly tokenManager: iTokenManager){}
+  
+  verifyToken(token: string){
+    if(!this.tokenManager || !this.tokenManager.verify)
+     throw new Error("Invalid TokenGenerator");
+
+    const verifyResult = this.tokenManager.verify(token);
+    
+    if(typeof verifyResult === "string")
+      return {statusCode: 401, body: verifyResult};
+  }
+}
+
+class TokenManagerSpy implements iTokenManager {
+  public verifyReturns : string | { userId: string} = { userId: "any_id"};
+
+  verify(token: string): string | { userId: string; } {
+    return this.verifyReturns;
+  }
+
+  generate(userId: string): string {
+    return userId;
+  }
+}
+
+const makeSut = () => {
+  const tokenManager = new TokenManagerSpy();
+  const sut = new AuthMiddleware(tokenManager);
+
+  return {sut, tokenManager};
+}
+
+
+describe('AuthMiddleware', () => {
+  test('should throw if invalid token manager has sent', () => {
+    const tokenManager = {} as iTokenManager;
+    const sut = new AuthMiddleware(tokenManager);
+
+    expect(()=>{sut.verifyToken("any_token")}).toThrow(new Error("Invalid TokenGenerator"))
+  })
+
+  test('should return 401 if invalid token is sent', () => {
+    const {tokenManager, sut} = makeSut();
+
+    tokenManager.verifyReturns = "token expired";
+
+    const httpResponse = sut.verifyToken("invalid_token") as HttpRes;
+    
+    expect(httpResponse.body).toBe("token expired");
+    expect(httpResponse.statusCode).toBe(401);
+  })
+})
