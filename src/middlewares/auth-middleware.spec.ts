9ea3df476ed7c03e2aa39a6169aa043c033e5785
WilghanerSilva <wilghener@gmail.com>
2023-01-20 21:15:06 -0300

    changing middleware params

--- src/middlewares/auth-middleware.spec.ts
+++ src/middlewares/auth-middleware.spec.ts
@@ -1,15 +1,28 @@
 import {iTokenManager} from '../utils/interfaces';
-import { HttpRes } from '../utils/types/Http-types';
+import { HttpRes, HttpReq } from '../utils/types/Http-types';
 
 class AuthMiddleware {
   constructor (private readonly tokenManager: iTokenManager){}
   
-  verifyToken(token: string){
+  verifyToken(httpRequest: HttpReq){
+    const {authorization} = httpRequest.headers;
+    const regex = new RegExp('Bearer');
+
+    if(!authorization)
+      return { statusCode: 401, body: "Missing token"};
+
     if(!this.tokenManager || !this.tokenManager.verify)
      throw new Error("Invalid TokenGenerator");
 
-    const verifyResult = this.tokenManager.verify(token);
-    
+    const authorizationSplit = authorization.split(" ");
+
+    if(!authorizationSplit)
+      return { statusCode: 401, body: "Unauthorized"};
+
+    if(!regex.test(authorizationSplit[0]))
+      return { statusCode: 401, body: "Invalid token"};
+
+    const verifyResult = this.tokenManager.verify(authorizationSplit[1]);
     if(typeof verifyResult === "string")
       return {statusCode: 401, body: verifyResult};
 
@@ -42,15 +55,27 @@ describe('AuthMiddleware', () => {
     const tokenManager = {} as iTokenManager;
     const sut = new AuthMiddleware(tokenManager);
 
-    expect(()=>{sut.verifyToken("any_token")}).toThrow(new Error("Invalid TokenGenerator"))
+    const httpReq = {
+      body: {},
+      headers: {
+        authorization: "Bearer any_token"
+      }
+    }
+
+    expect(()=>{sut.verifyToken(httpReq)}).toThrow(new Error("Invalid TokenGenerator"))
   })
 
-  test('should return 401 if invalid token is sent', () => {
-    const {tokenManager, sut} = makeSut();
+  test('should return 401 and invalid on body if invalid token is sent', () => {
+    const {sut} = makeSut();
 
-    tokenManager.verifyReturns = "token expired";
+    const httpReq = {
+      body: {},
+      headers: {
+        authorization: "invalid any_token"
+      }
+    }
 
-    const httpResponse = sut.verifyToken("invalid_token") as HttpRes;
+    const httpResponse = sut.verifyToken(httpReq) as HttpRes;
     
     expect(httpResponse.body).toBe("token expired");
     expect(httpResponse.statusCode).toBe(401);
@@ -59,7 +84,14 @@ describe('AuthMiddleware', () => {
   test("should return userId if correct token has sent", () => {
     const {sut} = makeSut();
 
-    const userId = sut.verifyToken("valid_token");
+    const httpReq = {
+      body: {},
+      headers: {
+        authorization: "Bearer any_token"
+      }
+    }
+
+    const userId = sut.verifyToken(httpReq);
 
     expect(typeof userId === "string").toBe(true);
   })
