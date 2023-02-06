ccba98e43f234e0f6a0eee606a16f8ac7eb54246
WilghanerSilva <wilghener@gmail.com>
2023-02-06 16:41:47 -0300

    ensuring that it throws an error if the userId is invalid and returns the surveyId if everything is ok

--- src/repositories/create-survey-reository.spec.ts
+++ src/repositories/create-survey-reository.spec.ts
@@ -1,6 +1,7 @@
 import {iCreateSurveyRepository }from "../utils/interfaces";
 import { prismaMock } from "../../singleton";
 import prisma from "../../client";
+import { Survey } from "@prisma/client";
 
 class CreateSurveyRepository implements iCreateSurveyRepository {
 	async create(userId: string): Promise<string> {
@@ -12,8 +13,20 @@ class CreateSurveyRepository implements iCreateSurveyRepository {
 
 		if(!user)
 			throw new Error("Invalid user id");
-	
-		return "any_survey_id";
+    
+		const survey = await prisma.survey.create({
+			data: {
+				author: {
+					connect: {
+						id: userId
+					}
+				}
+			},
+      
+      
+		});
+
+		return survey.id;
 	}
 }
 
@@ -25,5 +38,26 @@ describe("CreateSurveyRepository", () => {
 
 		expect(sut.create("any_id"))
 			.rejects.toThrow(new Error("Invalid user id"));
+	});
+
+	test("Shold return surveyId if everything is ok", async () => {
+		const sut = new CreateSurveyRepository();
+
+		const mockedSurvey : Survey = {
+			authorId: "any_id",
+			id: "any_id"
+		};
+
+		prismaMock.survey.create.mockResolvedValue(mockedSurvey);
+		prismaMock.user.findUnique.mockResolvedValue({
+			email: "any_email@mail.com",
+			name: "any_name",
+			id: "any_id",
+			password: "hashed_password"
+		});
+
+		const surveyId = await sut.create("any_id");
+
+		expect(surveyId).toEqual(mockedSurvey.id);
 	});
 });
